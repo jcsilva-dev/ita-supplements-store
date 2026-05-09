a28d20880155e7de710f2fb7bbf8e449517c9250
Jose carlos Silva <josecarlossilva@MacBook-Air-de-Jose.local>
2026-05-09 06:54:34 -0300

    configure SQLite database

--- app/settings.py
+++ app/settings.py
@@ -77,13 +77,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'suplementos',
-        'USER': 'postgres',
-        'PASSWORD':'Jose0026',
-        'HOST': 'localhost',
-        'PORT':'5432',
-         
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
